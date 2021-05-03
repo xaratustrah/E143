08756c219edbc501174ab734173197d829f58c4f
xaratustrah <shahab.sanjari@gmail.com>
2021-05-03 19:31:34 +0200

    currently ok

--- e143_looper.py
+++ e143_looper.py
@@ -15,13 +15,17 @@ import time
 import platform
 if platform.system() == 'Linux':
     import psutil
-
+import shutil
 from iqtools import *
 
+# How often to check for new files?
 SLEEP = 3  # [s]
 
 
 def process_loop(directory, wwwpath, logfilename):
+    """
+    main loop
+    """
     for file in os.listdir(directory):
         if file.lower().endswith('.tiq'):
             fullfilename = os.path.join(directory, file)
@@ -29,24 +33,15 @@ def process_loop(directory, wwwpath, logfilename):
                 if not has_handle(fullfilename):
                     process_each(fullfilename)
                     put_into_logfile(fullfilename, logfilename)
+                    copy_files_to_wwwpath(fullfilename, wwwpath)
 
 
 def process_each(filename):
+    """
+    what to do with each file
+    """
+
     print('Processing ', filename)
-    get_png_plot(filename)
-    get_root_file(filename)
-    copy_files_to_wwwpath()
-
-
-def copy_files_to_wwwpath():
-    pass
-
-
-def get_root_file(filename):
-    pass
-
-
-def get_png_plot(filename):
     iq = get_iq_object(filename)
     iq.read_samples(1)
     lframes = 1024
@@ -56,13 +51,39 @@ def get_png_plot(filename):
     xx, yy, zz = iq.get_spectrogram(nframes=nframes, lframes=lframes)
     plot_spectrogram(xx, yy, zz, cen=iq.center, filename=filename)
 
+    with uproot3.recreate(filename + '.root') as f:
+        f['info_tree'] = uproot3.newtree(
+            {'sampling_freq': uproot3.newbranch(np.int32, title='Sampling frequency')})
+        f['data_tree'] = uproot3.newtree(
+            {'power': uproot3.newbranch(np.float64, title='Time domain signal power')})
+        f['info_tree'].extend({'sampling_freq': np.array([int(iq.fs)])})
+        f['data_tree'].extend({'power': np.abs(iq.data_array)**2})
+
+
+def copy_files_to_wwwpath(filename, wwwpath):
+    """
+    copy the data on the mounted filesystem
+    """
+
+    shutil.copy(filename, wwwpath)
+    shutil.copy(filename + '.root', wwwpath)
+    shutil.copy(filename + '.png', wwwpath)
+
 
 def put_into_logfile(file, logfilename):
+    """
+    Write into the log file.
+    """
+
     with open(logfilename, 'a') as file_object:
         file_object.write(file + '\n')
 
 
 def already_processed(currentfilename, logfilename):
+    """
+    check whether the file is already in the log file
+    """
+
     already_processed = False
     try:
         with open(logfilename, 'r') as file_object:
@@ -79,9 +100,13 @@ def already_processed(currentfilename, logfilename):
 
 
 def has_handle(fpath):
-    # The following function thanks to:
-    # https://stackoverflow.com/a/44615315/5177935
-    # works only under linux
+    """
+    Check whether the file is not finished copying.
+    thanks to:
+    https://stackoverflow.com/a/44615315/5177935
+    works only under linux
+    """
+
     if platform.system() == 'Linux':
         for proc in psutil.process_iter():
             try:
@@ -104,7 +129,7 @@ def main():
     parser.add_argument('monitor_directory', type=str,
                         help='Name of the directory to monitor for files. Use full paths.')
     parser.add_argument('-l', '--logfile', nargs='?', type=str, default=default_logfilename,
-                        help='Name of the file for tracking the list of processed files. Use full paths.')
+                        help='Name of the file for tracking the list of processed files. Use full paths')
     parser.add_argument('-p', '--wwwpath', nargs='?', type=str, default='./',
                         help='Remote path to WWW server. Use full paths.')
 
@@ -120,13 +145,16 @@ def main():
 
     try:
         while True:
-            process_loop(args.monitor_directory, args.wwwpath, args.logfile)
+            # Make sure there is a trailing slash at the end of the path
+            monitor_directory = os.path.join(args.monitor_directory, '')
+            wwwpath = os.path.join(args.wwwpath, '')
+            process_loop(monitor_directory, wwwpath, args.logfile)
             time.sleep(SLEEP)
             print('I am waiting for new files...')
 
     except KeyboardInterrupt:
         print(
-            "Oh no! You don't want me to continue waiting for new files! Aborting you wish :-(")
+            "\nOh no! You don't want me to continue waiting for new files! Aborting as you wish :-(")
 
 
 # ------------------------
